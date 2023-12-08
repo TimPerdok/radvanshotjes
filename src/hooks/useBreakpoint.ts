433a1f29d08fd598b60d9dbf32d786328abc31e5
Tim Perdok <tim-perdok@hotmail.com>
2023-12-08 20:55:43 +0100

    init

--- src/hooks/useBreakpoint.ts
+++ src/hooks/useBreakpoint.ts
@@ -10,28 +10,27 @@ const breakpoints = {
 };
 
 const useBreakpoint = () => {
-  return "md";
-  // const [breakpoint, setBreakPoint] = useState('');
-  // const { height, width } = useWindowSize();
+  const [breakpoint, setBreakPoint] = useState('');
+  const { height, width } = useWindowSize();
 
-  // useEffect(() => {
-  //   if (0 < width && width < 600) {
-  //     setBreakPoint(breakpoints[0]);
-  //   }
-  //   if (600 < width && width < 960) {
-  //     setBreakPoint(breakpoints[600]);
-  //   }
-  //   if (960 < width && width < 1280) {
-  //     setBreakPoint(breakpoints[960]);
-  //   }
-  //   if (1280 < width && width < 1920) {
-  //     setBreakPoint(breakpoints[1280]);
-  //   }
-  //   if (width >= 1920) {
-  //     setBreakPoint(breakpoints[1920]);
-  //   }
-  // }, [width]);
-  // return breakpoint;
+  useEffect(() => {
+    if (0 < width && width < 600) {
+      setBreakPoint(breakpoints[0]);
+    }
+    if (600 < width && width < 960) {
+      setBreakPoint(breakpoints[600]);
+    }
+    if (960 < width && width < 1280) {
+      setBreakPoint(breakpoints[960]);
+    }
+    if (1280 < width && width < 1920) {
+      setBreakPoint(breakpoints[1280]);
+    }
+    if (width >= 1920) {
+      setBreakPoint(breakpoints[1920]);
+    }
+  }, [width]);
+  return breakpoint;
 };
 
 export default useBreakpoint;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,8 @@
 import React, { Component, useEffect, useMemo } from "react";
-import Layout from "../components/Layout";
 import { ThemeProvider } from "styled-components";
 import theme from "../data/theme";
 import { Theme } from "../types/types";
 import useBreakpoint from "../hooks/useBreakpoint";
-import Background from "../components/Background";
 import GlobalStyle from "../globalStyling";
 import Head from "next/head";
 import { Provider } from "react-redux";
@@ -15,52 +13,16 @@ import Loading from "../components/animations/Loading";
 export default function App({ Component, pageProps }) {
   const breakpoint = useBreakpoint();
  
-  
-
-  useEffect(() => {
-    // if ("serviceWorker" in navigator) {
-    //   navigator.serviceWorker
-    //     .register("./sw.js")
-    //     .then((registration) => {
-    //       console.log("service worker registration successful");
-    //     })
-    //     .catch((err) => {
-    //       console.warn("service worker registration failed", err.message);
-    //     });
-    // }
-
-
-    // if ('serviceWorker' in navigator) {
-    //   navigator.serviceWorker.ready.then(registration => {
-    //     registration.unregister();
-
-    //     if (caches) {
-    //       // Service worker cache should be cleared with caches.delete()
-    //       caches.keys().then(async (names) => {
-    //         await Promise.all(names.map(name => caches.delete(name)));
-    //       });
-    //     }
-    //   });
-    // }
-
-  }, []);
-
-
   return (
     <>
       <Head>
-        <title>Tim Perdok</title>
-
+        <title>Nieuwjaarspel</title>
       </Head>
       <Provider store={store}>
-
         <GlobalStyle />
-        
-        <Layout>
-          <ThemeProvider theme={{ ...theme, breakpoint } as Theme}>
-            <Component {...pageProps} />
-          </ThemeProvider>
-        </Layout>
+        <ThemeProvider theme={{ ...theme, breakpoint } as Theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </Provider>
 
     </>
