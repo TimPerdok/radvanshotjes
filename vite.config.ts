26fb857c7bc56743b3796901bfc76ad292f20299
Tim Perdok <tim-perdok@hotmail.com>
2024-11-04 21:41:36 +0100

    f

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import deno from '@deno/vite-plugin'
 import react from '@vitejs/plugin-react'
+import process from "node:process";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [deno(), react()],
+  base: process.env.VITE_BASE_PATH || '/',
   resolve: {
     alias: {
       '@': '/src',
